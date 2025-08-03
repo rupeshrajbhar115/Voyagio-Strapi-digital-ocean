cddb6c1e9f094285aab9ea1b0efccb18baa641f5
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-08-04 00:46:29 +0530

    Enable DigitalOcean Spaces endpoint configuration
    Uncommented the endpoint configuration for DigitalOcean Spaces in the plugins config, allowing the use of a custom endpoint URL for file uploads.

--- config/plugins.js
+++ config/plugins.js
@@ -22,7 +22,7 @@ module.exports = ({ env }) => ({
       providerOptions: {
         accessKeyId: env("DO_SPACE_ACCESS_KEY"), //allbuckets-1754230400792
         secretAccessKey: env("DO_SPACE_SECRET_KEY"), //oAYjk+LqJ91Fh+LNw9yhW1U/H+0Zu+wNnkbplPn37TQ
-        // endpoint: env("DO_SPACE_ENDPOINT"), // https://voyagio-imagestore1.sfo3.digitaloceanspaces.co
+        endpoint: env("DO_SPACE_ENDPOINT"), // https://voyagio-imagestore1.sfo3.digitaloceanspaces.com
         region: env("DO_SPACE_REGION"), //sfo3
         params: {
           Bucket: env("DO_SPACE_BUCKET"), //voyagio-imagestore1
