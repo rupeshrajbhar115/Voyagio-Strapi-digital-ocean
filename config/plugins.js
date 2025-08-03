c88c601ac7d0fea005e3834ab9d1852e803c6277
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-08-03 19:59:42 +0530

    Configure S3 upload provider and add dependency
    Switched upload provider configuration to use 'aws-s3' in plugins.js and added '@strapi/provider-upload-aws-s3' to dependencies in package.json and package-lock.json for S3 integration.

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,33 @@
-module.exports = () => ({});
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "@strapi/provider-upload-s3",
+//       providerOptions: {
+//         accessKeyId: env("DO_SPACE_ACCESS_KEY"), //allbuckets-1754230400792
+//         secretAccessKey: env("DO_SPACE_SECRET_KEY"), //oAYjk+LqJ91Fh+LNw9yhW1U/H+0Zu+wNnkbplPn37TQ
+//         endpoint: env("DO_SPACE_ENDPOINT"), // important  https://voyagio-imagestore1.sfo3.digitaloceanspaces.co
+//         region: env("DO_SPACE_REGION"), //sfo3
+//         params: {
+//           Bucket: env("DO_SPACE_BUCKET"), //voyagio-imagestore1
+//         },
+//       },
+//     },
+//   },
+// });
+
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"), //allbuckets-1754230400792
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"), //oAYjk+LqJ91Fh+LNw9yhW1U/H+0Zu+wNnkbplPn37TQ
+        endpoint: env("DO_SPACE_ENDPOINT"), // https://voyagio-imagestore1.sfo3.digitaloceanspaces.co
+        region: env("DO_SPACE_REGION"), //sfo3
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"), //voyagio-imagestore1
+        },
+      },
+    },
+  },
+});
