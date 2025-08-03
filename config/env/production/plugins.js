080ec529e5487fc09f1341eb8c7ced307e88ea0d
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-08-03 20:17:57 +0530

    Remove sensitive comments from S3 config
    Eliminated comments containing sensitive information such as access keys, secret keys, and bucket details from the production upload plugin configuration for improved security.

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,14 +1,31 @@
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "aws-s3",
+//       providerOptions: {
+//         accessKeyId: env("DO_SPACE_ACCESS_KEY"), //allbuckets-1754230400792
+//         secretAccessKey: env("DO_SPACE_SECRET_KEY"), //oAYjk+LqJ91Fh+LNw9yhW1U/H+0Zu+wNnkbplPn37TQ
+//         endpoint: env("DO_SPACE_ENDPOINT"), // https://voyagio-imagestore1.sfo3.digitaloceanspaces.co
+//         region: env("DO_SPACE_REGION"), //sfo3
+//         params: {
+//           Bucket: env("DO_SPACE_BUCKET"), //voyagio-imagestore1
+//         },
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   upload: {
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("DO_SPACE_ACCESS_KEY"), //allbuckets-1754230400792
-        secretAccessKey: env("DO_SPACE_SECRET_KEY"), //oAYjk+LqJ91Fh+LNw9yhW1U/H+0Zu+wNnkbplPn37TQ
-        endpoint: env("DO_SPACE_ENDPOINT"), // https://voyagio-imagestore1.sfo3.digitaloceanspaces.co
-        region: env("DO_SPACE_REGION"), //sfo3
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        region: env("DO_SPACE_REGION"),
         params: {
-          Bucket: env("DO_SPACE_BUCKET"), //voyagio-imagestore1
+          Bucket: env("DO_SPACE_BUCKET"),
         },
       },
     },
