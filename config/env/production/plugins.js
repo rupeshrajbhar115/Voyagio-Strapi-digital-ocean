62149a460da19b1831d952936524aa02a966614c
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-08-03 20:05:48 +0530

    Add AWS S3 upload plugin config for production
    Introduces production environment configuration for the upload plugin using AWS S3 provider with DigitalOcean Spaces credentials.

---
+++ config/env/production/plugins.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"), //allbuckets-1754230400792
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"), //oAYjk+LqJ91Fh+LNw9yhW1U/H+0Zu+wNnkbplPn37TQ
+        endpoint: env("DO_SPACE_ENDPOINT"), // https://voyagio-imagestore1.sfo3.digitaloceanspaces.co
+        region: env("DO_SPACE_REGION"), //sfo3
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"), //voyagio-imagestore1
+        },
+      },
+    },
+  },
+});
